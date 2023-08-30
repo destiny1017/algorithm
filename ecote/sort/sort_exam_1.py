66a28e1d3938d9fc4db4d2b04111bbf987e97e36
KDH_Notebook <destiny1017@nate.com>
2023-08-30 20:57:38 +0900

    정렬 연습

--- ecote/sort/sort_exam_1.py
+++ ecote/sort/sort_exam_1.py
@@ -11,3 +11,27 @@ for i in range(len(arr)):
 
 print("정렬 후 : ", arr)
 
+
+# 선택정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr)
+
+
+# 삽입정렬
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+        else:
+            break
+
+print(arr)
+
--- ecote/sort/sort_exam_4.py
+++ ecote/sort/sort_exam_4.py
@@ -33,3 +33,30 @@ def quick(start, end):
 quick(0, len(arr)-1)
 print("정렬 후 : ", arr)
 
+
+# 퀵정렬2
+
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+print("정렬 전 : ", arr)
+
+
+def quick(array):
+    if len(array) <= 1:
+        return array
+
+    left_arr, mid_arr, right_arr = [], [], []
+    pivot = array[len(array) // 2]
+
+    for i in array:
+        if pivot > i:
+            left_arr.append(i)
+        elif pivot < i:
+            right_arr.append(i)
+        else:
+            mid_arr.append(i)
+
+    return quick(left_arr) + mid_arr + quick(right_arr)
+
+
+print("정렬 후 : ", quick(arr))
