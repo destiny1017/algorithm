ffcf4a1d972a3887e7511c2b67ccbf956ad517fe
KDH_Notebook <destiny1017@nate.com>
2023-09-28 16:47:23 +0900

    14502 연구소 (최종)

--- baekjoon/14502_연구소.py
+++ baekjoon/14502_연구소.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 import copy
+from itertools import combinations
 
 sys.setrecursionlimit(10 ** 6)
 
@@ -9,69 +10,64 @@ board = []
 cnt = 0
 dx = [0, 1, -1, 0]
 dy = [1, 0, 0, -1]
+wall_able = []
+virus = deque()
 max_safe_count = 0
+safe_cnt = 0
 
 for i in range(n):
     board.append(list(map(int, sys.stdin.readline().split())))
 
+# 바이러스 좌표 구하기
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 2:
+            virus.append((i, j))
 
-def print_road(road):
-    for i in range(n):
-        print(road[i])
-    print("------------------------")
+# 안전 영역 개수 구하기
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 0:
+            safe_cnt += 1
+            wall_able.append((i, j))
+
+# 안전 영역에서 추가되는 벽 개수만큼 빼기
+safe_cnt -= 3
 
 
 def bfs(road, queue):
-    while len(queue) > 0:
+    global safe_cnt
+    global max_safe_count
+    safe = safe_cnt
+
+    while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            # print(nx, ny)
             if n <= nx or nx < 0 or m <= ny or ny < 0:
-                # print(nx, ny, "continued")
                 continue
             if road[nx][ny] != 0:
                 continue
+
+            safe -= 1
+            if safe < max_safe_count:
+                return
             road[nx][ny] = 3
             queue.append((nx, ny))
 
-
-def safe_count(road):
-    global max_safe_count
-    safe_cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if road[i][j] == 0:
-                safe_cnt += 1
-    if max_safe_count < safe_cnt:
-        max_safe_count = safe_cnt
+    max_safe_count = max(max_safe_count, safe)
 
 
-def contagion(road):
-    for i in range(n):
-        for j in range(m):
-            if road[i][j] == 2:
-                queue = deque()
-                queue.append((i, j))
-                bfs(road, queue)
-    return safe_count(road)
+# 3개의 벽 설치가 가능한 모든 경우의 수 구하기
+wall_combinations = combinations(wall_able, 3)
 
+# 벽 설치 경우의 수만큼 bfs 수행
+for wall in wall_combinations:
+    road = copy.deepcopy(board)
+    queue = copy.deepcopy(virus)
+    for x, y in wall:
+        road[x][y] = 1
 
-def rounds(road, depth):
-    for i in range(n):
-        for j in range(m):
-            if road[i][j] != 1 and road[i][j] != 2:
-                if depth < 3:
-                    road[i][j] = 1
-                    if depth == 2:
-                        # print_road(road)
-                        contagion(copy.deepcopy(road))
-                        # print(safe_count)
-                        # global cnt
-                        # cnt += 1
-                    rounds(road, depth + 1)
-                    road[i][j] = 0
+    bfs(road, queue)
 
-
-rounds(board, 0)
 print(max_safe_count)
