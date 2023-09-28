a97f72d9610472c2774cbebceea8194b36788db3
KDH_Notebook <destiny1017@nate.com>
2023-09-28 21:49:37 +0900

    2464 안전 영역(메모리 초과)

---
+++ baekjoon/2464_안전영역.py
@@ -0,0 +1,56 @@
+# https://www.acmicpc.net/problem/2468
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+frame = []
+flooded = []
+dx, dy = [0, 1, -1, 0], [1, 0, 0, -1]
+
+max_height = 0
+min_height = 100
+max_safe_zone = 0
+
+for i in range(n):
+    frame.append(list(map(int, sys.stdin.readline().split())))
+
+for row in frame:
+    max_height = max(max_height, max(row))
+    min_height = min(min_height, min(row))
+
+
+def bfs(px, py, water_depth):
+    queue = deque()
+    queue.append((px, py))
+
+    while queue:
+        x, y = queue.popleft()
+        flooded[x][y] = True
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if nx >= n or nx < 0 or ny >= n or ny < 0:
+                continue
+            if water_depth >= frame[nx][ny]:
+                continue
+            if flooded[nx][ny]:
+                continue
+            queue.append((nx, ny))
+
+
+for water_depth in range(min_height, max_height):
+    flooded = [[False] * n for _ in range(n)]
+    safe_zone = 0
+
+    for i in range(n):
+        for j in range(n):
+            if not flooded[i][j] and water_depth < frame[i][j]:
+                bfs(i, j, water_depth)
+                safe_zone += 1
+
+    # print(f"depth = {water_depth}, safe zone = {safe_zone}")
+    max_safe_zone = max(max_safe_zone, safe_zone)
+
+
+print(max_safe_zone)
